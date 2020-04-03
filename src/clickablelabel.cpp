9c55f408ac5923c5df5cff6dd7d960529bc218f5
Carles Pina i Estany <carles@pina.cat>
2020-04-03 22:38:41 +0200

    Adds missing licensing information

--- src/clickablelabel.cpp
+++ src/clickablelabel.cpp
@@ -5,6 +5,24 @@
 #include <QPainter>
 #include <QDebug>
 
+/*
+ * Copyright 2017, 2020 Carles Pina i Estany <carles@pina.cat>
+ * This file is part of qnetload.
+ *
+ * qnetload is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * qnetload is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with qnetload.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 ClickableLabel::ClickableLabel(QWidget* parent, Qt::WindowFlags f)
     : QLabel(parent)
     , m_inside(false)
