ef2419cf2f41e480cb9d3aa225c2f43ed25048df
Carles Pina i Estany <carles@pina.cat>
2023-10-02 22:30:45 +0100

    Update version and years of copyright

--- src/main.cpp
+++ src/main.cpp
@@ -27,9 +27,9 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     QApplication::setOrganizationName("qnetload");
     QApplication::setApplicationName("qnetload");
-    QApplication::setApplicationVersion("1.2");
+    QApplication::setApplicationVersion("1.3");
 
-    qInfo() << qUtf8Printable("qnetload version " + QApplication::applicationVersion() + " Copyright (C) 2017, 2019, 2020 Carles Pina i Estany <carles@pina.cat>");
+    qInfo() << qUtf8Printable("qnetload version " + QApplication::applicationVersion() + " Copyright (C) 2017, 2019, 2020, 2021, 2023 Carles Pina i Estany <carles@pina.cat>");
     qInfo() << qUtf8Printable("This program comes with ABSOLUTELY NO WARRANTY");
     qInfo() << qUtf8Printable("This is free software, and you are welcome to redistribute it under certain conditions");
     qInfo() << qUtf8Printable("License: GPL v3");
